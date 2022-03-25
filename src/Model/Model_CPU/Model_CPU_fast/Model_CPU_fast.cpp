d9aaacf570d1fef8822eb674f73f029dc819ad7c
Zijie NING <zijie.ning@imt-atlantique.net>
2022-03-25 12:02:55 +0100

    version 3, reduced select

--- src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
+++ src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
@@ -1,4 +1,4 @@
-// #define GALAX_MODEL_CPU_FAST 1
+#define GALAX_MODEL_CPU_FAST 1
 #ifdef GALAX_MODEL_CPU_FAST
 
 #include <cmath>
@@ -14,9 +14,9 @@ namespace xs = xsimd;
 using b_type = xs::batch<float, xs::avx2>;
 using b_bool = xs::batch_bool<b_type>;
 
-b_type threshold = 0.0001;
-b_type b_zero = 0.0;
-b_type b = 10.0;
+// b_type threshold = 0.0001;
+// b_type b_zero = 0.0;
+// b_type b = 10.0;
 // b_type rposx_j;
 // b_type rposy_j;
 // b_type rposz_j;
@@ -33,7 +33,7 @@ Model_CPU_fast ::Model_CPU_fast(const Initstate &initstate, Particles &particles
 }
 
 // ! choose version
-#define VERSION 2
+#define VERSION 3
 
 #if VERSION == 1
 void forward(int n_particles, const Initstate &initstate, Particles &particles, std::vector<float> &velocitiesx, std::vector<float> &velocitiesy, std::vector<float> &velocitiesz, std::vector<float> &accelerationsx, std::vector<float> &accelerationsy, std::vector<float> &accelerationsz)
@@ -124,9 +124,9 @@ void forward(int n_particles, const Initstate &initstate, Particles &particles, 
                 b_type rvely_j = b_type::load_unaligned(&velocitiesy[j]);
                 b_type rvelz_j = b_type::load_unaligned(&velocitiesz[j]);
 
-                // b_type threshold = 0.0001;
-                // b_type b_zero = 0.0;
-                // b_type b = 10.0;
+                b_type threshold = 0.0001;
+                b_type b_zero = 0.0;
+                b_type b = 10.0;
 
                 // std::cout << "rposx_i" << rposx_i << std::endl;
                 // std::cout << "rposy_i" << rposy_i << std::endl;
@@ -230,6 +230,218 @@ void forward(int n_particles, const Initstate &initstate, Particles &particles, 
 void forward(int n_particles, const Initstate &initstate, Particles &particles, std::vector<float> &velocitiesx, std::vector<float> &velocitiesy, std::vector<float> &velocitiesz, std::vector<float> &accelerationsx, std::vector<float> &accelerationsy, std::vector<float> &accelerationsz)
 {
 
+    // ! Use Xsimd
+
+    std::size_t inc = b_type::size;
+#pragma omp parallel for
+    for (int i = 0; i < n_particles -inc+1; i += inc)
+    {
+        // load registers body i
+        b_type rposx_i = b_type::load_unaligned(&particles.x[i]); // ? const
+        b_type rposy_i = b_type::load_unaligned(&particles.y[i]);
+        b_type rposz_i = b_type::load_unaligned(&particles.z[i]);
+        b_type raccx_i = b_type::load_unaligned(&accelerationsx[i]);
+        b_type raccy_i = b_type::load_unaligned(&accelerationsy[i]);
+        b_type raccz_i = b_type::load_unaligned(&accelerationsz[i]);
+        b_type rvelx_i = b_type::load_unaligned(&velocitiesx[i]);
+        b_type rvely_i = b_type::load_unaligned(&velocitiesy[i]);
+        b_type rvelz_i = b_type::load_unaligned(&velocitiesz[i]);
+
+
+        for (int j = 0; j < n_particles -inc + 1; j += 1)
+        {
+            if (i != j)
+            {
+                // std::cout << "i" << i << std::endl;
+                // std::cout << "j" << j << std::endl;
+
+                // load registers body j
+                b_type rposx_j = b_type::load_unaligned(&particles.x[j]);
+                b_type rposy_j = b_type::load_unaligned(&particles.y[j]);
+                b_type rposz_j = b_type::load_unaligned(&particles.z[j]);
+                b_type raccx_j = b_type::load_unaligned(&accelerationsx[j]);
+                b_type raccy_j = b_type::load_unaligned(&accelerationsy[j]);
+                b_type raccz_j = b_type::load_unaligned(&accelerationsz[j]);
+                b_type rvelx_j = b_type::load_unaligned(&velocitiesx[j]);
+                b_type rvely_j = b_type::load_unaligned(&velocitiesy[j]);
+                b_type rvelz_j = b_type::load_unaligned(&velocitiesz[j]);
+
+                b_type b = 10.0;
+
+                // std::cout << "rposx_i" << rposx_i << std::endl;
+                // std::cout << "rposy_i" << rposy_i << std::endl;
+                // std::cout << "rposz_i" << rposz_i << std::endl;
+
+                // std::cout << "rposx_j" << rposx_j << std::endl;
+                // std::cout << "rposy_j" << rposy_j << std::endl;
+                // std::cout << "rposz_j" << rposz_j << std::endl;
+
+                b_type diffx = rposx_j - rposx_i;
+                b_type diffy = rposy_j - rposy_i;
+                b_type diffz = rposz_j - rposz_i;
+
+                // std::cout << "diffx" << diffx << std::endl;
+                // std::cout << "diffy" << diffy << std::endl;
+                // std::cout << "diffz" << diffz << std::endl;
+
+                b_type dij = diffx * diffx + diffy * diffy + diffz * diffz;
+
+                
+                // dij = xs::rsqrt(dij); // rsqrt=1/sqrt
+                // dij = xs::select(xs::gt(b, dij), b, 10.0 * dij * dij * dij);
+
+                b_type c = xs::rsqrt(dij); // rsqrt=1/sqrt
+                /// dij = 1.0;
+                dij = xs::fmin(b, 10.0 * c * c * c);
+                // std::cout << "dij" << dij << std::endl;
+                
+
+
+
+                
+                b_type raccx_i_j = diffx * dij;
+                b_type raccy_i_j = diffy * dij;
+                b_type raccz_i_j = diffz * dij;
+                // std::cout << "raccx_i_j" << raccx_i_j << std::endl;
+                // std::cout << "raccy_i_j" << raccy_i_j << std::endl;
+                // std::cout << "raccz_i_j" << raccz_i_j << std::endl;
+
+                raccx_i = raccx_i + raccx_i_j * initstate.masses[j];
+                raccy_i = raccy_i + raccy_i_j * initstate.masses[j];
+                raccz_i = raccz_i + raccz_i_j * initstate.masses[j];
+
+                // raccx_j = raccx_j - raccx_i_j * initstate.masses[i];
+                // raccy_j = raccy_j - raccy_i_j * initstate.masses[i];
+                // raccz_j = raccz_j - raccz_i_j * initstate.masses[i];
+
+                
+                
+            }
+        }
+
+        for (int j = n_particles -inc + 1; j < n_particles; j += 1)
+        {
+            if (i != j)
+            {
+                // std::cout << "i" << i << std::endl;
+                // std::cout << "j" << j << std::endl;
+
+                // load registers body j
+                b_type rposx_j = b_type::load_unaligned(&particles.x[j]);
+                b_type rposy_j = b_type::load_unaligned(&particles.y[j]);
+                b_type rposz_j = b_type::load_unaligned(&particles.z[j]);
+                b_type raccx_j = b_type::load_unaligned(&accelerationsx[j]);
+                b_type raccy_j = b_type::load_unaligned(&accelerationsy[j]);
+                b_type raccz_j = b_type::load_unaligned(&accelerationsz[j]);
+                b_type rvelx_j = b_type::load_unaligned(&velocitiesx[j]);
+                b_type rvely_j = b_type::load_unaligned(&velocitiesy[j]);
+                b_type rvelz_j = b_type::load_unaligned(&velocitiesz[j]);
+
+                b_type threshold = 0.0001;
+                b_type b_zero = 0.0;
+                b_type b = 10.0;
+
+                // std::cout << "rposx_i" << rposx_i << std::endl;
+                // std::cout << "rposy_i" << rposy_i << std::endl;
+                // std::cout << "rposz_i" << rposz_i << std::endl;
+
+                // std::cout << "rposx_j" << rposx_j << std::endl;
+                // std::cout << "rposy_j" << rposy_j << std::endl;
+                // std::cout << "rposz_j" << rposz_j << std::endl;
+
+                b_type diffx = rposx_j - rposx_i;
+                b_type diffy = rposy_j - rposy_i;
+                b_type diffz = rposz_j - rposz_i;
+
+                // std::cout << "diffx" << diffx << std::endl;
+                // std::cout << "diffy" << diffy << std::endl;
+                // std::cout << "diffz" << diffz << std::endl;
+
+                b_type dij = diffx * diffx + diffy * diffy + diffz * diffz;
+
+                
+                // dij = xs::rsqrt(dij); // rsqrt=1/sqrt
+                // dij = xs::select(xs::gt(b, dij), b, 10.0 * dij * dij * dij);
+
+                b_type c = xs::rsqrt(dij); // rsqrt=1/sqrt
+                /// dij = 1.0;
+                dij = xs::fmin(b, 10.0 * c * c * c);
+                // std::cout << "dij" << dij << std::endl;
+                
+
+
+
+
+                // b_type not_zero = bitwise_and(xs::gt(rposx_i, 1e-4),xs::gt(rposx_i, 1e-4));
+                // b_bool not_zero = xs::bitwise_and(xs::gt(b, a),xs::gt(a, b));
+
+           
+
+
+                auto not_zero =xs::bitwise_and(xs::bitwise_and(xs::gt(xs::abs(rposx_j), threshold),xs::gt(xs::abs(rposy_j), threshold)),xs::gt(xs::abs(rposz_j), threshold));
+                dij = xs::select(not_zero, dij, b_zero);
+
+             
+
+                b_type raccx_i_j = diffx * dij;
+                b_type raccy_i_j = diffy * dij;
+                b_type raccz_i_j = diffz * dij;
+                // std::cout << "raccx_i_j" << raccx_i_j << std::endl;
+                // std::cout << "raccy_i_j" << raccy_i_j << std::endl;
+                // std::cout << "raccz_i_j" << raccz_i_j << std::endl;
+
+                raccx_i = raccx_i + raccx_i_j * initstate.masses[j];
+                raccy_i = raccy_i + raccy_i_j * initstate.masses[j];
+                raccz_i = raccz_i + raccz_i_j * initstate.masses[j];
+
+                // raccx_j = raccx_j - raccx_i_j * initstate.masses[i];
+                // raccy_j = raccy_j - raccy_i_j * initstate.masses[i];
+                // raccz_j = raccz_j - raccz_i_j * initstate.masses[i];
+
+                
+                
+            }
+        }
+
+
+        raccx_i.store_unaligned(&accelerationsx[i]);
+        raccy_i.store_unaligned(&accelerationsy[i]);
+        raccz_i.store_unaligned(&accelerationsz[i]);
+
+        // raccx_j.store_unaligned(&accelerationsx[j]);
+        // raccy_j.store_unaligned(&accelerationsy[j]);
+        // raccz_j.store_unaligned(&accelerationsz[j]);
+
+        
+
+        //         rvelx_i += raccx_i * 2.0f;
+        //         rvely_i += raccy_i * 2.0f;
+        //         rvelz_i += raccz_i * 2.0f;
+
+        //         rvelx_i.store_unaligned(&velocitiesx[i]);
+        //         rvely_i.store_unaligned(&velocitiesy[i]);
+        //         rvelz_i.store_unaligned(&velocitiesz[i]);
+
+        //         rposx_i += rvelx_i * 0.1f;
+        //         rposy_i += rvely_i * 0.1f;
+        //         rposz_i += rvelz_i * 0.1f;
+
+        //         rposx_i.store_unaligned(&particles.x[i]);
+        //         rposy_i.store_unaligned(&particles.y[i]);
+        //         rposz_i.store_unaligned(&particles.z[i]);
+    }
+
+    #pragma omp parallel for
+    for (int i = 0; i < n_particles; i++)
+    {
+        velocitiesx[i] += accelerationsx[i] * 2.0f;
+        // if (i==0) std::cout << "accelerationsx_" << i << " = " << accelerationsx[i] << std::endl;
+        velocitiesy[i] += accelerationsy[i] * 2.0f;
+        velocitiesz[i] += accelerationsz[i] * 2.0f;
+        particles.x[i] += velocitiesx[i] * 0.1f;
+        particles.y[i] += velocitiesy[i] * 0.1f;
+        particles.z[i] += velocitiesz[i] * 0.1f;
+    }
 }
 #else
     std::cerr << "Not implemented!" << std::endl;
